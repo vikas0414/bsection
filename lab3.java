77408364820aaccbfa67d9261660b215e7f49029
vikas0414 <vvce22ise0031@vvce.ac.in>
2025-03-04 15:01:59 +0530

    Update lab3.java

--- lab3.java
+++ lab3.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask the user to enter two numbers
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Add the two numbers
+        double sum = num1 + num2;
+
+        // Output the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        
+        // Close the scanner object
+        scanner.close();
+    }
+}
 
